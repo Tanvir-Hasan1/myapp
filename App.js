7309a59a73a78e9382bbbfe0d51458cf9f10a744
Tanvir762 <93593858+Tanvir762@users.noreply.github.com>
2025-04-25 16:29:19 +0600

    welcome page build

--- App.js
+++ App.js
@@ -7,67 +7,14 @@ import {
   Platform, 
   StatusBar 
 } from 'react-native';
+import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      
-      {/* Top 80% Image Background Section */}
-      <ImageBackground 
-        source={{ uri: 'https://picsum.photos/600/800' }} 
-        style={styles.imageBackground}
-        resizeMode="cover"
-      >
-        <View style={styles.logoContainer}>
-          <Image 
-            source={require('./app/assets/logo.png')} 
-            style={styles.logo} 
-          />
-          <Text style={styles.logoText}>Set what you don’t need</Text>
-        </View>
-      </ImageBackground>
-
-      {/* Bottom Bars */}
-      <View style={styles.yellowBar} />
-      <View style={styles.blueBar} />
-    </View>
+    <WelcomeScreen />
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
-    backgroundColor: 'black',
-  },
-  imageBackground: {
-    flex: 0.8, // 80% of the screen height
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  logoContainer: {
-    alignItems: 'center',
-  },
-  logo: {
-    width: 100,
-    height: 100,
-    marginBottom: 10,
-    boxShadowColor: 'yellow',
-
-  },
-  logoText: {
-    color: 'white',
-    fontSize: 16,
-    marginTop: 5,
-  },
-  yellowBar: {
-    flex: 0.1, // 10% of the screen height
-    backgroundColor: 'yellow',
-    borderTopLeftRadius: 20,
-  },
-  blueBar: {
-    flex: 0.1, // 10% of the screen height
-    backgroundColor: 'blue',
-    borderTopLeftRadius: 20,
-  },
+  
 });
--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import { ImageBackground, StyleSheet } from 'react-native';
+import { Image, ImageBackground, Platform, StyleSheet, Text, View, StatusBar } from 'react-native';
 
 function WelcomeScreen(props) {
     return (
-        <ImageBackground>
-           source={require("../assets/welcome.jpeg")}
-           style={styles.background} 
+        <ImageBackground
+            source={require("../assets/welcome.jpeg")}
+            style={styles.background} 
+        >
+            <View style={styles.logoContainer}>
+                <Image style={styles.logo} source={require('../assets/logo.png')}/>
+                <Text>What we need here</Text>
+            </View>
+
+            <View style={styles.login}></View>
+            <View style={styles.signup}></View>
+           
         </ImageBackground>
     );
 }
@@ -13,7 +22,31 @@ function WelcomeScreen(props) {
 export default WelcomeScreen;
 const styles = StyleSheet.create({
     background:{
-        flex: 1,
+        flex: 1, 
+        justifyContent: "flex-end",
+        alignItems: "center",
+        paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    },
+    login:{
+        backgroundColor: "#fc56f6",
+        width: "100%",
+        height: 70,
+    },
+    signup:{
+        backgroundColor: "#4ecdc4",
+        width: "100%",
+        height: 70,
+    },
+    logo:{
+        
+        width: 200,
+        height: 200,
+    },
+    logoContainer:{
+        alignItems: "center", 
+        position: "absolute",
+        top: 70,
         
     }
 })
+
